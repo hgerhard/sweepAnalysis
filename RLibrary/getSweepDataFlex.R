b1afc2e149ea2e1dc6a2f62ea20533a2fb335d91
Nathan <kongnathan@gmail.com>
2015-12-31 21:09:18 -0500

    Minor changes -- more return values so data is more informative

--- RLibrary/getSweepDataFlex.R
+++ RLibrary/getSweepDataFlex.R
@@ -1,31 +1,71 @@
-getSweepDataFlex <- function(datafile, chanToKeep = c(), condToKeep = c())
+getSweepDataFlex <- function(datafile, chanToKeep = c())
 {
-	allData  <- read.table(datafile, header=TRUE)
-	colNames <- colnames(allData)
-	colsToKeep <- c(2, 3, 4, 5, 11, 19, 22, 21, 24, 20, 28, 30, 13, 14)
-	colsToKeepNames <- c()
+	allData  <- read.table(datafile, stringsAsFactors = FALSE, header=TRUE);
+	colNames <- colnames(allData);
+	
+	# 19 signal, 20 phase
+	#colsToKeep <- c(2, 3, 4, 5, 11, 19, 22, 21, 24, 20, 28, 30, 13, 14);
+	colsToKeep <- c(2, 3, 4, 5, 7, 12, 19, 20, 21);
+	colsToKeepNames <- c();
+
+	maxFreq <- allData$iFr[which.max(allData$iFr)];
+	freqs <- c();
+	for (i in 1:maxFreq)
+	{
+		freqs = c(freqs, i);
+	}
+	
+	allData <- allData[, colsToKeep];
 
 	for (i in 1:length(colsToKeep))
 	{
-		colsToKeepNames[i] = colNames[i]
+		colsToKeepNames[i] = colNames[colsToKeep[i]];
 	}
+	
+	# List implementation
+	# channelDict = c();
+	# currChanNum = 1;
+	# for (i in 1:dim(allData)[1])
+	# {
+		# currChannel = allData$iCh[i];
+		# if (currChannel %in% channelDict)
+		# {
+			# allData$iCh[i] = match(allData$iCh[i], channelDict);
+		# }
+		# else
+		# {
+			# channelDict <- c(channelDict, currChannel);
+			# allData$iCh[i] = match(allData$iCh[i], channelDict);
+		# }
+	# }
+	
+	# Env implementation for map
+	channelDict <- new.env(hash=TRUE);
+	currChanNum = 1;
+	for (i in 1:dim(allData)[1])
+	{
+		key = allData$iCh[i];
+		if (exists(key, channelDict))
+		{
+			allData$iCh[i] = channelDict[[key]];
+		}
+		else
+		{
+			channelDict[[key]] <- currChanNum;
+			allData$iCh[i] = channelDict[[key]];
+			currChanNum = currChanNum + 1;
+		}
+	}
+	
 	if (length(chanToKeep) != 0)
 	{
 		for (i in 1:length(chanToKeep))
 		{
-			currChan = chanToKeep[i]
-			allData = allData[allData$iCh == currChan]
-		}
-	}
-	if (length(condToKeep) != 0)
-	{
-		for (i in 1:length(condToKeep))
-		{
-			currCond = condToKeep[i]
-			allData = allData[allData$iCond == currCond]
+			currChan = chanToKeep[i];
+			allData = allData[allData$iCh == currChan];
 		}
 	}
 
-	returnVals <- list("allData" = allData, "colsKeptNames" = colsToKeepNames)
-	return(returnVals)
+	returnVals <- list("allData" = allData, "colsKeptNames" = colsToKeepNames, "freqs" = freqs, "channelDict" = channelDict);
+	return(returnVals);
 }
