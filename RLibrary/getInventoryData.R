3b44f0059a8ad1dc99f5cf0c008d002a167c09c2
Nathan <kongnathan@gmail.com>
2016-01-07 09:19:24 -0500

    More clarifications and print R generated summary statistics

--- RLibrary/getInventoryData.R
+++ RLibrary/getInventoryData.R
@@ -1,15 +1,18 @@
 getInventoryData <- function(datafile)
 {
+	# NAs mean missing data, it is different from "N/A" in the inventory data
+	
 	require(xlsx);
 	allData <- read.xlsx(datafile, sheetName = "CVI", stringsAsFactors = FALSE, header = TRUE);
 	colHeaders = colnames(allData);
-	questionHeaders = colHeaders[7:59];
+	questionHeaders = c(colHeaders[7:59]);
 
 	# 7:59 is hard coded question indices in the data file
 	averageScorePerSubject = c();
 	sdScorePerSubject = c();
 	maxScorePerSubject = c();
 	minScorePerSubject = c();
+	medianScorePerSubject = c();
 	for (i in 1:dim(allData)[1])
 	{
 		currData <- suppressWarnings(as.numeric(allData[i, 7:59]));
@@ -17,15 +20,29 @@ getInventoryData <- function(datafile)
 		currSD <- sd(currData, na.rm = TRUE);
 		currMax <- suppressWarnings(max(currData, na.rm = TRUE));
 		currMin <- suppressWarnings(min(currData, na.rm = TRUE));
+		currMedian <- suppressWarnings(median(currData, na.rm = TRUE));
 		averageScorePerSubject = c(averageScorePerSubject, currMean);
 		sdScorePerSubject = c(sdScorePerSubject, currSD);
 		maxScorePerSubject = c(maxScorePerSubject, currMax);
 		minScorePerSubject = c(minScorePerSubject, currMin);
+		medianScorePerSubject = c(medianScorePerSubject, currMedian);
 	}
 	allData$averageScores <- averageScorePerSubject;
 	allData$stdScores <- sdScorePerSubject;
-	allData$maxScore <- maxScorePerSubject;
-	allData$minScore <- minScorePerSubject;
+	allData$maxScores <- maxScorePerSubject;
+	allData$minScores <- minScorePerSubject;
+	allData$medianScores <- medianScorePerSubject;
+
+	for (question in 1:length(questionHeaders))
+	{
+		currQuestion = questionHeaders[question];
+		cat("Question", currQuestion, "\n");
+		# Hard coded 6 here, since questions start at index 7
+		currData = allData[question+6];
+		cat("\n")
+		cat(summary(currData));
+		cat("\n\n");
+	}
 
 	returnVals <- list("allData" = allData, "questionHeaders" = questionHeaders);
 	return(returnVals);
