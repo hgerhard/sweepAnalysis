1d696b294a3341565586f07970849d7af6b25de0
Nathan <kongnathan@gmail.com>
2016-01-07 08:58:52 -0500

    More descriptive stats

--- RLibrary/getInventoryData.R
+++ RLibrary/getInventoryData.R
@@ -6,14 +6,26 @@ getInventoryData <- function(datafile)
 	questionHeaders = colHeaders[7:59];
 
 	# 7:59 is hard coded question indices in the data file
-	averageScorePerQuestion = c();
+	averageScorePerSubject = c();
+	sdScorePerSubject = c();
+	maxScorePerSubject = c();
+	minScorePerSubject = c();
 	for (i in 1:dim(allData)[1])
 	{
 		currData <- suppressWarnings(as.numeric(allData[i, 7:59]));
 		currMean <- mean(currData, na.rm = TRUE);
-		averageScorePerQuestion = c(averageScorePerQuestion, currMean);
+		currSD <- sd(currData, na.rm = TRUE);
+		currMax <- suppressWarnings(max(currData, na.rm = TRUE));
+		currMin <- suppressWarnings(min(currData, na.rm = TRUE));
+		averageScorePerSubject = c(averageScorePerSubject, currMean);
+		sdScorePerSubject = c(sdScorePerSubject, currSD);
+		maxScorePerSubject = c(maxScorePerSubject, currMax);
+		minScorePerSubject = c(minScorePerSubject, currMin);
 	}
-	allData$averageScores <- averageScorePerQuestion
+	allData$averageScores <- averageScorePerSubject;
+	allData$stdScores <- sdScorePerSubject;
+	allData$maxScore <- maxScorePerSubject;
+	allData$minScore <- minScorePerSubject;
 
 	returnVals <- list("allData" = allData, "questionHeaders" = questionHeaders);
 	return(returnVals);
