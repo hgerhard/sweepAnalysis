2458f558593d209047ba6c5a0f2381681c9d42a0
Nathan <kongnathan@gmail.com>
2016-01-14 00:08:02 -0500

    More features added.

--- RLibrary/getSweepDataFlex.R
+++ RLibrary/getSweepDataFlex.R
@@ -1,60 +1,22 @@
 getSweepDataFlex <- function(datafile, chanToKeep = c())
 {
-	allData  <- read.table(datafile, stringsAsFactors = FALSE, header=TRUE);
+	allData  <- read.table(datafile, stringsAsFactors=TRUE, header=TRUE);
 	colNames <- colnames(allData);
 	
 	# 19 signal, 20 phase
-	#colsToKeep <- c(2, 3, 4, 5, 11, 19, 22, 21, 24, 20, 28, 30, 13, 14);
-	colsToKeep <- c(2, 3, 4, 5, 7, 12, 19, 20, 21);
+	colsToKeep <- c(2, 3, 4, 5, 7, 9, 11, 12, 19, 20, 21, 22, 24, 28, 30, 13, 14);
+	#colsToKeep <- c(2, 3, 4, 5, 7, 12, 19, 20, 21);
 	colsToKeepNames <- c();
 
-	maxFreq <- allData$iFr[which.max(allData$iFr)];
-	freqs <- c();
-	for (i in 1:maxFreq)
-	{
-		freqs = c(freqs, i);
-	}
+	# maxFreq <- allData$iFr[which.max(allData$iFr)];
+	freqsAnalyzed <- c();
+	freqsAnalyzed <- levels(allData$Harm);
 	
 	allData <- allData[, colsToKeep];
 
 	for (i in 1:length(colsToKeep))
 	{
 		colsToKeepNames[i] = colNames[colsToKeep[i]];
-	}
-	
-	# List implementation
-	# channelDict = c();
-	# currChanNum = 1;
-	# for (i in 1:dim(allData)[1])
-	# {
-		# currChannel = allData$iCh[i];
-		# if (currChannel %in% channelDict)
-		# {
-			# allData$iCh[i] = match(allData$iCh[i], channelDict);
-		# }
-		# else
-		# {
-			# channelDict <- c(channelDict, currChannel);
-			# allData$iCh[i] = match(allData$iCh[i], channelDict);
-		# }
-	# }
-	
-	# Env implementation for map
-	channelDict <- new.env(hash=TRUE);
-	currChanNum = 1;
-	for (i in 1:dim(allData)[1])
-	{
-		key = allData$iCh[i];
-		if (exists(key, channelDict))
-		{
-			allData$iCh[i] = channelDict[[key]];
-		}
-		else
-		{
-			channelDict[[key]] <- currChanNum;
-			allData$iCh[i] = channelDict[[key]];
-			currChanNum = currChanNum + 1;
-		}
 	}
 	
 	if (length(chanToKeep) != 0)
@@ -66,6 +28,21 @@ getSweepDataFlex <- function(datafile, chanToKeep = c())
 		}
 	}
 
-	returnVals <- list("allData" = allData, "colsKeptNames" = colsToKeepNames, "freqs" = freqs, "channelDict" = channelDict);
+	# Get amplitudes
+	# Looks like amplitudes is just the same as "Signal" (column 19 in data)
+	amplitudes = c();
+	signalReal = allData$Sr;
+	signalImag = allData$Si;
+	stopifnot(length(signalReal) == length(signalImag));
+	stopifnot(length(signalReal) == dim(allData)[1]);
+	for (i in 1:dim(allData)[1])
+	{
+		currAmpl = sqrt(signalReal[i]^2 + signalImag[i]^2);
+		amplitudes = c(amplitudes, currAmpl);
+	}
+	allData$ampl = amplitudes;
+	colsToKeepNames = colnames(allData);
+
+	returnVals <- list("selectedData" = allData, "colsKeptNames" = colsToKeepNames, "freqsAnalyzed" = freqsAnalyzed);
 	return(returnVals);
 }
